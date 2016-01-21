bca5025427a32167803af434bea324d02addb6f6
Virgile Daugé <virgile.dauge@telecomnancy.net>
2016-01-21 11:20:11 +0100

    clean plus poussé

--- app/main_client.js
+++ app/main_client.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_ONLY',
-});
-
 console.log('Running on client only');
 
 Meteor.startup(() => {
